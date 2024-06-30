7ca8b54cd89441d409d3245da709dff3538a6df3
charles Gift <charlesgiftangila@gmail.com>
2024-06-30 22:29:55 +0300

    readme

--- speed.js
+++ speed.js
@@ -1,7 +1,7 @@
 function speedChecker() {
-  let speed = prompt("Enter speed of car");
+  let speed = prompt("Enter speed of car"); //prompts the user to key in speed of car.
   if (isNaN(speed)) {
-    alert("Error!! Enter a valid speed!");
+    alert("Error!! Enter a valid speed!"); //alerts the user when the value keyed in is not a number
     return;
   }
 
@@ -14,7 +14,7 @@ function speedChecker() {
     const demeritPoints = Math.abs((speed - speedLimit) / demeritPointsPerKm);
 
     if (demeritPoints > 12) {
-      alert("Licence Suspended");
+      alert("Licence Suspended"); //alerts the user if the speed limit has been exceeded
     } else {
       alert(`Points: ${demeritPoints}`);
     }
